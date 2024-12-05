08d996f4a47b3f839f9e18dfe8bfa8575581538d
rkyarc <arickyh1@gmail.com>
2024-12-06 05:34:20 +0700

    update description for challenge 5

--- 5-zeroFuel.js
+++ 5-zeroFuel.js
@@ -1,3 +1,7 @@
+// coding challenge 2024
+// challenge 5
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+
 // const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     if(distanceToPump / mpg <= fuelLeft) {
 //         return true
@@ -6,9 +10,9 @@
 //     }
 // };
 
-function zeroFuel(distanceToPump, mpg, fuelLeft) {
-    return (distanceToPump > mpg * fuelLeft) ? false : true;
-}
+// function zeroFuel(distanceToPump, mpg, fuelLeft) {
+//     return (distanceToPump > mpg * fuelLeft) ? false : true;
+// }
 
 // const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
 //     return distanceToPump / mpg <= fuelLeft;
@@ -18,6 +22,6 @@ function zeroFuel(distanceToPump, mpg, fuelLeft) {
 //     return fuelLeft * mpg >= distanceToPump;
 // };
 
-// const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump / mpg <= fuelLeft;
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump / mpg <= fuelLeft;
 
   console.log(zeroFuel(100, 50, 1));
