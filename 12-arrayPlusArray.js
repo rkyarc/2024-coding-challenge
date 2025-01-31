4488c304d451e36383659be366d81650c81a812b
rkyarc <arickyh1@gmail.com>
2025-01-31 20:29:50 +0700

    challenge 12

---
+++ 12-arrayPlusArray.js
@@ -0,0 +1,27 @@
+// coding challenge 2024
+// challenge 12
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+
+// function arrayPlusArray(arr1, arr2) {
+    // let total = 0;
+
+    // for (let i = 0; i < arr1.length; i++) {
+    //     total += arr1[i];
+    // }
+
+    // for (let i = 0; i < arr2.length; i++) {
+    //     total += arr2[i];
+    // }
+
+    // for(let i = 0; i < arguments.length; i++) {
+    //     for(let j = 0; j < arguments[i].length; j++) {
+    //         total+= arguments[i][j]
+    //     }
+    // }
+
+    // return [...arr1, ...arr2].reduce((acc, curr) => acc + curr) 
+// }
+
+const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((acc, curr) => acc + curr);
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]))
