59af60152478e54ab351b6985c929acf96b167e1
rkyarc <arickyh1@gmail.com>
2025-02-07 21:57:36 +0700

    challenge 25

---
+++ 25-doubleChar.js
@@ -0,0 +1,17 @@
+// coding challenge 2024
+// challenge 24
+// https://www.codewars.com/kata/56b1f01c247c01db92000076
+
+// function doubleChar(str) {
+//     let result = '';
+
+//     for (let i = 0; i < str.length; i++) {
+//         result += str[i]
+//         result += str[i]
+//     }
+//     return result;
+// }
+
+const doubleChar = (str) => str.split('').map((el) => el + el).join('');
+
+console.log(doubleChar("Adidas"))
