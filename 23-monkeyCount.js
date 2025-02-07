6fca9471a65a01f1cffb3c5e351d24d2179c19c8
rkyarc <arickyh1@gmail.com>
2025-02-07 21:18:00 +0700

    challenge 23

---
+++ 23-monkeyCount.js
@@ -0,0 +1,16 @@
+// coding challenge 2024
+// challenge 23
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+// function monkeyCount(n) {
+//     const result = [];
+
+//     for (let i = 1; i <= n; i++) {
+//         result.push(i)
+//     }
+//     return result;
+// }
+
+const monkeyCount = (n) => Array.from({ length: n}, (_,i) => i + 1);
+
+console.log(monkeyCount(10))
